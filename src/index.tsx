2e79fc222279f39e90e1f0d6be13ee66f5735138
Jin <ming-jin.yeh@rxpservices.com>
2021-06-25 15:03:08 +1000

    feature: prepare build

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import Button from "./components/Button/Button";
 import Icon from "./components/Icon/Icon";
+import CodeBlock from "./components/CodeBlock/CodeBlock";
+import Spinner from "./components/Spinner/Spinner";
 
-export { Button, Icon };
+export { Button, Icon, Spinner, CodeBlock };
